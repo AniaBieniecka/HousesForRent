97e13f413d6c226756a599ea26993cf281234634
Ania Bieniecka <annabieniecka5@gmail.com>
2024-04-03 14:26:10 +0200

    Created database using EF

---
+++ HousesForRent.Infrastructure/Migrations/20240403121108_AddHouseToDb.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HousesForRent.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHouseToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Houses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    DiscountPrice = table.Column<double>(type: "float", nullable: false),
+                    Occupancy = table.Column<int>(type: "int", nullable: false),
+                    RoomsQuantity = table.Column<int>(type: "int", nullable: false),
+                    SingleBedQuantity = table.Column<int>(type: "int", nullable: false),
+                    DoubleBedQuantity = table.Column<int>(type: "int", nullable: false),
+                    Area = table.Column<int>(type: "int", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Houses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Houses");
+        }
+    }
+}
