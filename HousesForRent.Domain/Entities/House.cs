249b715235a64a43697b102f8c97f5c67c103b2a
Ania Bieniecka <annabieniecka5@gmail.com>
2024-04-05 19:49:44 +0200

    Created actions and views: Index and Create

--- HousesForRent.Domain/Entities/House.cs
+++ HousesForRent.Domain/Entities/House.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +13,31 @@ namespace HousesForRent.Domain.Entities
         public required string Name { get; set; }
         public string? Description { get; set; }
         public string Location { get; set; }
+        [Display(Name = "Price per night")]
+        [Range(0, double.MaxValue)]
+
         public double Price { get; set; }
+        [Display(Name = "Discount price per night")]
+        [Range(0, double.MaxValue)]
         public double DiscountPrice { get; set; }
-        public int Occupancy { get; set; }        
+        public int Occupancy { get; set; }
+        [Display(Name = "Number of rooms")]
+        [Range(0, int.MaxValue)]
+
         public int RoomsQuantity { get; set; }
+        [Display(Name = "Number of single beds")]
+        [Range(0, int.MaxValue)]
         public int SingleBedQuantity { get; set; }
+        [Display(Name = "Number of double beds")]
+        [Range(0, int.MaxValue)]
         public int DoubleBedQuantity { get; set; }
-        public int Area {  get; set; }
+        [Display(Name = "Area in square meters")]
+        [Range(0, int.MaxValue)]
+        public int Area { get; set; }
+        [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreatedDate { get; set; }
-        public DateTime? UpdatedDate { get; set;}
+        public DateTime? UpdatedDate { get; set; }
 
     }
 }
---
+++ HousesForRent.Web/Controllers/HouseController.cs
@@ -0,0 +1,42 @@
+﻿using HousesForRent.Application.Common.Interfaces;
+using HousesForRent.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousesForRent.Web.Controllers
+{
+    public class HouseController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HouseController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var houses = _unitOfWork.House.GetAll().ToList();
+            return View(houses);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(House house)
+        {
+            if(house.Price < house.DiscountPrice)
+            {
+                ModelState.AddModelError("DiscountPrice", "The discount price cannot be higer than standard price");
+            }
+            if(ModelState.IsValid)
+            {
+            _unitOfWork.House.Add(house);
+            _unitOfWork.House.Save();
+            return RedirectToAction("Index");
+            }
+            else return View(house);
+        }
+    }
+}
