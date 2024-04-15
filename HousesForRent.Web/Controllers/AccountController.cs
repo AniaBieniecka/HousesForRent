a65f0a230e20dd4a1585fd958c8894648608df82
Ania Bieniecka <annabieniecka5@gmail.com>
2024-04-15 19:49:12 +0200

    Created account controller and login view

---
+++ HousesForRent.Web/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using HousesForRent.Application.Common.Interfaces;
+using HousesForRent.Domain.Entities;
+using HousesForRent.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousesForRent.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _RoleManager;
+        public AccountController(IUnitOfWork unitOfWork,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _unitOfWork = unitOfWork;
+            _RoleManager = roleManager;
+            _signInManager = signInManager;
+            _RoleManager = roleManager;
+        }
+        public IActionResult Login(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            LoginVM loginVM = new()
+            {
+                RedirectUrl = returnUrl
+            };
+
+            return View(loginVM);
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ HousesForRent.Web/ViewModels/LoginVM.cs
@@ -0,0 +1,19 @@
+﻿using HousesForRent.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace HousesForRent.Web.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+        public string? RedirectUrl { get; set; }
+    }
+}
