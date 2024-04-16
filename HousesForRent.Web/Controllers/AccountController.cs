2db0821b997d044427b171fcc65f2c770ec66e07
Ania Bieniecka <annabieniecka5@gmail.com>
2024-04-16 23:49:25 +0200

    Created Logout action

--- HousesForRent.Web/Controllers/AccountController.cs
+++ HousesForRent.Web/Controllers/AccountController.cs
@@ -131,5 +131,12 @@ namespace HousesForRent.Web.Controllers
 
             return View(registerVM);
         }
+
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
