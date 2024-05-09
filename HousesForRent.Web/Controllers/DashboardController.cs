bca1d2954e2aba2e75ce575a56b65e31c0e1a976
Ania Bieniecka <annabieniecka5@gmail.com>
2024-05-09 22:09:47 +0200

    Created first radial chart (total bookings) in dashboard

--- HousesForRent.Web/Controllers/DashboardController.cs
+++ HousesForRent.Web/Controllers/DashboardController.cs
@@ -1,12 +1,75 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HousesForRent.Application.Common.Interfaces;
+using HousesForRent.Application.Common.Utility;
+using HousesForRent.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HousesForRent.Web.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        static int previousMonth = DateTime.Now.Month == 1 ? 12 : DateTime.Now.Month - 1;
+        readonly DateTime previousMonthStartDate = new(DateTime.Now.Year, previousMonth, 1);
+        readonly DateTime currentMonthStartDate = new(DateTime.Now.Year, DateTime.Now.Month, 1);
+
+        public DashboardController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;   
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> GetTotalBookingChartData()
+        {
+            var totalBookings = _unitOfWork.Booking.GetAll(u => u.Status != SD.StatusPending || u.Status==SD.StatusCancelled);
+
+            double countByCurrentMonth = totalBookings.Count(u => u.BookingDate >= currentMonthStartDate && 
+            u.BookingDate <= DateTime.Now);
+
+            double countByPreviousMonth = totalBookings.Count(u => u.BookingDate >= previousMonthStartDate &&
+            u.BookingDate <= currentMonthStartDate);
+
+            var radialBarChartVM = GetRadialChartViewModel(totalBookings.Count(), countByCurrentMonth, countByPreviousMonth);
+
+            return Json(radialBarChartVM);
+        }
+
+        public async Task<IActionResult> GetTotalUserChartData()
+        {
+            var totalUsers = _unitOfWork.ApplicationUser.GetAll();
+
+            double countByCurrentMonth = totalUsers.Count(u => u.CreatedAt >= currentMonthStartDate &&
+            u.CreatedAt <= DateTime.Now);
+
+            double countByPreviousMonth = totalUsers.Count(u => u.CreatedAt >= previousMonthStartDate &&
+            u.CreatedAt <= currentMonthStartDate);
+
+            var radialBarChartVM = GetRadialChartViewModel(totalUsers.Count(), countByCurrentMonth, countByPreviousMonth);
+
+            return Json(radialBarChartVM);
+        }
+
+        private static RadialBarChartVM GetRadialChartViewModel(int totalCount, double countByCurrentMonth, double countByPreviousMonth)
+        {
+            RadialBarChartVM radialBarChartVM = new();
+
+            int increaseDecreaseRatio = 100;
+
+            if (countByPreviousMonth != 0)
+            {
+                increaseDecreaseRatio = Convert.ToInt32((countByCurrentMonth - countByPreviousMonth) / countByPreviousMonth * 100);
+            }
+
+            radialBarChartVM.TotalCount = totalCount;
+            radialBarChartVM.CountCurrentMonth = Convert.ToInt32(countByCurrentMonth);
+            radialBarChartVM.MonthlyChange = Convert.ToInt32(countByCurrentMonth - countByPreviousMonth);
+            radialBarChartVM.Series = new int[] { increaseDecreaseRatio };
+
+            return radialBarChartVM;
         }
     }
 }
---
+++ HousesForRent.Web/ViewModels/RadialBarChartVM.cs
@@ -0,0 +1,10 @@
+﻿namespace HousesForRent.Web.ViewModels
+{
+    public class RadialBarChartVM
+    {
+        public decimal TotalCount { get; set; }
+        public decimal CountCurrentMonth { get; set; }
+        public int MonthlyChange { get; set; }
+        public int[] Series { get; set; }
+    }
+}
