8615412b75654a08de0b3582254e037031a69eb5
Ania Bieniecka <annabieniecka5@gmail.com>
2024-06-10 22:20:09 +0200

    Changed customer list

--- HousesForRent.Web/ViewModels/UserVM.cs
+++ HousesForRent.Web/ViewModels/UserVM.cs
@@ -9,6 +9,7 @@
         public string PhoneNumber { get; set; }
         public DateTime CreatedAt { get; set; }
         public IList<string> Roles { get; set; }
+        public int BookingsQty { get; set; }
 
     }
 }
